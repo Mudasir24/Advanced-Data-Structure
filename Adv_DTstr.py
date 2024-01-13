b95e0b75467e19ed97ffbfbb312b085704b7439e
Mudasir24 <151111505+Mudasir24@users.noreply.github.com>
2024-01-13 16:52:24 +0530

    Add files via upload

---
+++ Adv_DTstr.py
@@ -0,0 +1,9 @@
+# Storing customer info using dictionary
+customer_info = {"name" : "Rahul", "age" : 24, "purchase_history" : ['Books','Vegetables','Clothes','Laptop']}
+
+
+# Printing the customer's name
+print("The customer's name is ",customer_info["name"])
+
+# Printing the customers second purchase from the purchase history.
+print(customer_info["name"],"'s secound purchase is ",customer_info["purchase_history"][1])
